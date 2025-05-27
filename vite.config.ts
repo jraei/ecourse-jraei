efe41e070cda148f43af117113f66c3065647077
Justin <justinereifanwijaya@gmail.com>
2025-05-27 20:22:39 +0700

    fixing

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+        port: 8080,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
